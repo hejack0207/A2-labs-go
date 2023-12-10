1aaf1830885be9ed842617f743b87387fc5ad0bf
hejack <hejack0207@sina.com>
2023-12-10 22:19:19 +0800

    fix

---
+++ codes/lang-features/app/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+import "testing"
+
+func panicRecover() {
+	defer catch()
+	triggerPanic()
+}
+
+func catch() {
+	if err := recover(); err != nil {
+		fmt.Println("catch the panic:", err)
+	}
+}
+
+func triggerPanic() {
+	panic("error occurred!")
+}
+
+func TestPanicRecover(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+}
+
+func main() {
+}
